92cd90ed8dc3a9ac8c6b8fa8756d7d86f042ab2f
patriziomunzi <patriziomunzi@gmail.com>
2015-08-29 13:12:37 +0200

    fixed loader

---
+++ js/build/services/message.js
@@ -0,0 +1,14 @@
+define(['angular','services/device'], function(angular) {
+    'use strict';
+    angular.module('wutsapp.services.Message', ['wutsapp.services.Device'])
+        .factory('Message', function(Device) {
+            var Message = function(json) {
+                this.id = json.id;
+                this.text = json.text;
+                this.you = json.you;
+                this.timestamp = Device.getTimestamp() + (Math.random() + this.id) * 60000;
+            };
+
+            return Message;
+        });
+});
